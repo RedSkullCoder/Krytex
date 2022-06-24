a8dead5d0331c18802d59897f7875697a14c4d62
Abdiel Carrasco <redskullcoder@gmail.com>
2022-06-24 02:26:13 -0700

    KRYTEX03 - SWAP SUCCESS

---
+++ migrations/1_initial_migration.js
@@ -0,0 +1,13 @@
+const Migrations = artifacts.require("Migrations");
+const DaiTokenMock = artifacts.require("DaiTokenMock");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Migrations);
+  await deployer.deploy(DaiTokenMock);
+  const tokenMock = await DaiTokenMock.deployed()
+  // Mint 1,000 Dai Tokens for the deployer
+  await tokenMock.mint(
+    '0xaF59749f07FF5699459224d49d88d90366d18b0F',
+    '1000000000000000000000'
+  )
+};
